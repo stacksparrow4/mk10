6768f82e93264a578050b16cde42a9436559f212
stacksparrow4 <stacksparrow4@gmail.com>
2022-12-14 15:45:24 +1100

    Removed wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,6 @@ mod types;
 use solve::solve;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
 #[wasm_bindgen]
 pub fn solve_problem(a: i32, b: i32, c: i32, d: i32) -> String {
     console_error_panic_hook::set_once();
